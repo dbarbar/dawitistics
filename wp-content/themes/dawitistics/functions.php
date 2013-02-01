c8085b750c24c1347800241ef346951570797775
Jeremy Green <jgreen@fiercemarkets.com>
2013-02-01 14:13:29 -0500

    random dawitistic

--- wp-content/themes/dawitistics/functions.php
+++ wp-content/themes/dawitistics/functions.php
@@ -1,2 +1,31 @@
 <?php
 remove_action('wp_head', 'wp_generator');
+
+//adding the api
+add_action('init', 'add_api');
+function add_api()
+{
+	global $wp_rewrite;
+
+	add_filter('query_vars', create_function('$qv', '$qv[] = "txt"; return $qv;'));
+	add_rewrite_rule('dawitistic\.txt$', $wp_rewrite->index.'?txt=1', 'top');
+
+	flush_rewrite_rules(false);
+}
+
+
+//check to see if the request is for text
+add_action('template_redirect', 'txt_template_redirect', 8);
+function is_txt()
+{
+	return (bool)get_query_var('txt');
+}
+
+function txt_template_redirect()
+{
+	if(is_txt())
+	{
+		load_template( TEMPLATEPATH . '/utils/dawitistic.php');
+		exit();
+	}
+}
---
+++ wp-content/themes/dawitistics/utils/dawitistic.php
@@ -0,0 +1,17 @@
+<?php
+//bootstrap wordpress
+require_once $_SERVER['DOCUMENT_ROOT'] . '/wp-blog-header.php';
+
+//get random post
+query_posts('orderby=rand&posts_per_page=1');
+
+//loop through post
+while(have_posts())
+{
+	the_post();
+	$title = get_the_title();
+}
+
+//send it
+header("Content-type: text/plain; charset: UTF-8");
+echo $title;
